9f9f486ad9a8e976c0993d1c18d723aa1e2d93a6
Martin Rodriguez <merodriguezblanco@gmail.com>
2017-11-07 14:43:33 -0300

    add jsx to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|jsx)$/,
         exclude: /(node_modules|bower_components)/,
         use: {
           loader: 'babel-loader',
